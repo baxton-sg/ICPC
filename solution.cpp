c83c95f4989c98c88bc7d8f46e359e02ef36d93d
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2015-10-16 11:58:47 +0800

    v6.1
    reduces scope of iterating by applying logic from "2 eggs and 100 storey building" task

--- solution.cpp
+++ solution.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
-#include <set>
+#include <algorithm>
+#include <cmath>
 
 
 using namespace std;
@@ -17,7 +18,7 @@ typedef vector<INT> storage;
 int cmp_impl(const INT* ring, int N, int beg1, int end1, int beg2, int end2) {
 
     int res = 0;
-    while(!res && beg1 != end1) {
+    while(0 == res && beg1 != end1) {
         res = ring[beg1] - ring[beg2];
         beg1 = (beg1 + 1) % N;
         beg2 = (beg2 + 1) % N;
@@ -41,15 +42,72 @@ struct idx_less {
     bool operator() (int i1, int i2) {
         int e1 = (i1 + size) % N;
         int e2 = (i2 + size) % N;
-        int res = cmp_impl(ring, N, i1, e1, i2, e2);
-        return 0 > res;
+        int res = (ring[i1] < ring[i2]) || 0 > cmp_impl(ring, N, i1, e1, i2, e2);
+        return res;
     }
 };
 
 
+template<int cmp(const INT*, int, int, int, int, int)>
+void get_start_stop(const INT* ring, int N, const vector<int>& indices, int& start_idx, int& stop_idx, int part_size, int part_size_num, int part_size_small) {
+    int step = (int)sqrt((double(N) - .5) * 2.);
+
+    bool found = false;
+    int b;
+    int i = step;
+    for (; i < N; i += step) {
+        b = indices[i];
+        int e = (b + part_size) % N; 
+
+        int full_parts = part_size_num - 1;
+        int full_parts_small = part_size_small;
+
+        int tmp_b = e;
+        int tmp_e = (tmp_b + part_size) % N;
+        int space = part_size;
+
+
+        while (full_parts) {
+
+            int res = cmp(ring, N, b, e, tmp_b, tmp_e);
+
+            if (0 <= res) {
+                full_parts -= 1;
+                space += part_size;
+                tmp_b = tmp_e;
+            }
+            else {
+                if (0 == full_parts_small)
+                    break;
+                space += part_size - 1;
+                full_parts_small -= 1;
+                tmp_b = (tmp_b + (part_size - 1)) % N;
+            }
+            tmp_e = (tmp_b + part_size) % N;
+
+        }   // while true
+            
+        // last one partition
+        if (0 == full_parts) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        start_idx = i - step;
+        stop_idx = i;
+    }
+    else {
+        start_idx = i;
+        stop_idx = N;
+    }
+}
+
+
 // 3 <= N && N <= 100000
 // 2 <= K && K <= N
-template<int cmp(const INT*, int, int, int, int, int/*, int*/)>
+template<int cmp(const INT*, int, int, int, int, int)>
 void solve(const INT* ring, int N, int K) {
     if (K == N) {
         int m = ring[0];
@@ -83,15 +141,26 @@ void solve(const INT* ring, int N, int K) {
         }
 
 
-        set<int, idx_less> indices(idx_less(ring, N, part_size));
-        for (int i = 0; i < N; ++i)
-            indices.insert(i);
+        vector<int> indices;
+        indices.reserve(N);
+        for (int i = 0; i < N; ++i) {
+            indices.push_back(i);
+        }
+        sort(indices.begin(), indices.end(), idx_less(ring, N, part_size));
 
 
-        int min_beg = 0, min_end = 0;
+        int start_idx = 0, stop_idx = 0;
+        get_start_stop<cmp_impl>(ring, N, indices, start_idx, stop_idx, part_size, part_size_num, part_size_small);
 
-        for (set<int>::const_iterator it = indices.begin(); it != indices.end(); ++it) {
-            int b = *it;
+
+        int min_beg = indices[stop_idx], 
+            min_end = (min_beg + part_size) % N;
+
+        int not_found = 0;
+        int found = 0;
+
+        for (int i = start_idx; i < stop_idx; ++i) {
+            int b = indices[i];
             int e = (b + part_size) % N; // increment(b, N, part_size);
 
 
@@ -104,11 +173,6 @@ void solve(const INT* ring, int N, int K) {
                         continue;
                 }
             }
-
-
-            //begins[0] = b;
-            //begins[1] = e;
-
 
             int full_parts = part_size_num - 1;
             int full_parts_small = part_size_small;
@@ -144,7 +208,6 @@ void solve(const INT* ring, int N, int K) {
                 min_end = e; 
                 break;
             }
-               
         }
 
 
