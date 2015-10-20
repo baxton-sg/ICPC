2abbd000c0b377eb3d52f3d9cd8e7ac964f1f210
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2015-10-20 15:30:58 +0800

    test

--- solution.cpp
+++ solution.cpp
@@ -1,105 +1,150 @@
 
-
-
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <ctime>
+
+#include <map>
+//#include <unordered_set>
 #include <algorithm>
-#include <cmath>
+//#include <cstring>
 
 
 using namespace std;
 
 
-typedef unsigned long long INT;
+typedef int INT;
+typedef vector<INT> storage;
 
 
 
-
-
-struct params {
-    int N;
-    int K;
-    
-    int part_size;
-    int part_size_num;
-    int part_size_small_num;
-
-    int num_per_INT;
-    INT last_mask;
-    INT first_mask;    
-
-    INT* ring;
-
-    ~params() {
-        delete [] ring;
-    }
+/*
+struct time_data {
+    int cnt;
+    double clocks;
+    time_data() :
+        cnt(0),
+        clocks(0.)
+    {}
 };
 
+struct timer {
+    static map<string, time_data> timers;
 
-int cmp(const params& p, int beg1, int beg2) {
+    string name;
+    clock_t start;
+    
+    timer(const char* timer_name) :
+        name(timer_name),
+        start(clock())
+    {}
 
-    if (p.part_size <= sizeof(INT)) {
-        return p.ring[beg1] - p.ring[beg2];
+
+    ~timer() {
+        time_data& td = timers[name];
+        td.cnt += 1;
+        td.clocks += double(clock() - start);
     }
+    
+    static ostream& print(ostream& os) {
+        for (map<string, time_data>::const_iterator it = timers.begin(); it != timers.end(); ++it) {
+            const string& name = it->first;
+            const time_data& td = it->second;
 
-    int size = p.part_size / sizeof(INT);
-    bool last_exists = p.part_size % sizeof(INT);
+            os << name << ": total sec " << (td.clocks / CLOCKS_PER_SEC) << " cnt " << td.cnt << " (avr: " << ((td.clocks / td.cnt) / CLOCKS_PER_SEC) << ")" << endl;
+        }
+        return os;
+    }    
+};
+map<string, time_data> timer::timers; 
+*/
 
-    for (int i = 0; i < size; ++i) {
-        if (p.ring[beg1] > p.ring[beg2])
-            return 1;
-        else if (p.ring[beg1] < p.ring[beg2])
-            return -1;
 
-        beg1 = (beg1 + p.num_per_INT) % p.N;
-        beg2 = (beg2 + p.num_per_INT) % p.N;
-    } 
 
-    if (last_exists) {
-        if ((p.ring[beg1] & p.last_mask) > (p.ring[beg2] & p.last_mask))
-            return 1;
-        else if ((p.ring[beg1] & p.last_mask) < (p.ring[beg2] & p.last_mask))
-            return -1;
+int cmp_impl(const INT* ring, int N, int beg1, int end1, int beg2, int end2/*, int size*/) {
+
+//timer tm("cmp");
+
+    /*
+    int res;
+    int size4 = size / 4;
+    for (int i = 0; i < size4; ++i) {
+        res = ring[beg1] - ring[beg2];
+        if (0 < res) return 1;
+        else if (0 > res) return -1;
+        beg1 = (beg1 + 1) % N;
+        beg2 = (beg2 + 1) % N;    
+     
+        res = ring[beg1] - ring[beg2];
+        if (0 < res) return 1;
+        else if (0 > res) return -1;
+        beg1 = (beg1 + 1) % N;
+        beg2 = (beg2 + 1) % N;   
+        
+        res = ring[beg1] - ring[beg2];
+        if (0 < res) return 1;
+        else if (0 > res) return -1;
+        beg1 = (beg1 + 1) % N;
+        beg2 = (beg2 + 1) % N;   
+        
+        res = ring[beg1] - ring[beg2];
+        if (0 < res) return 1;
+        else if (0 > res) return -1;
+        beg1 = (beg1 + 1) % N;
+        beg2 = (beg2 + 1) % N;          
     }
-
+    for (int i = size4*4; i < size; ++i) {
+        res = ring[beg1] - ring[beg2];
+        if (0 < res) return 1;
+        else if (0 > res) return -1;
+        beg1 = (beg1 + 1) % N;
+        beg2 = (beg2 + 1) % N;           
+    }
     return 0;
+    */
+    
+    
+    int res = ring[beg1] - ring[beg2];
+    while(!res && beg1 != end1) {
+        beg1 = (beg1 + 1) % N;
+        beg2 = (beg2 + 1) % N;
+        res = ring[beg1] - ring[beg2];
+    }
+    return res;
+    
 }
 
 
 
 struct idx_less {
-    const params& p;
-    idx_less(const params& par) :
-        p(par)
+    const INT* ring;
+    int size;
+    int N;
+    idx_less(const INT* v, int v_size, int part_size) :
+        ring(v),
+        size(part_size),
+        N(v_size)
     {}
 
     bool operator() (int i1, int i2) {
-        return 0 > cmp(p, i1, i2);
+        int e1 = (i1 + size) % N;
+        int e2 = (i2 + size) % N;
+        int res = cmp_impl(ring, N, i1, e1, i2, e2);
+        return 0 > res;
     }
 };
 
 
-
-ostream& print(ostream& os, const params& p, int b, int e) {
-    int shift = p.part_size <= sizeof(INT) ? (p.part_size - 1) * 8 : (sizeof(INT) - 1) * 8;
-    for (int z = b; z != e; z = (z + 1) % p.N) 
-        cout << (INT)((p.ring[z] & p.first_mask) >> shift);
-    cout << endl;
-    return os;
-}
-
-
-
 // 3 <= N && N <= 100000
 // 2 <= K && K <= N
-void solve(params& p) {
-    if (p.K == p.N) {
-        int m = p.ring[0];
+template<int cmp(const INT*, int, int, int, int, int/*, int*/)>
+void solve(const INT* ring, int N, int K) {
+    if (K == N) {
+        int m = ring[0];
         if (m < 9) {
-            for (int i = 0; i < p.N; ++i) {
-                if (m < p.ring[i]) {
-                    m = p.ring[i];
+            for (int i = 0; i < N; ++i) {
+                if (m < ring[i]) {
+                    m = ring[i];
                     if (m == 9)
                         break;
                 } 
@@ -109,154 +154,107 @@ void solve(params& p) {
     }
     else {
 
-/*
-for (int i = 0; i < p.N; ++i) {
-    cout << "[" << i << "] ";
-    for (int j = sizeof(INT) - 1; j >= 0; --j) {
-        INT v = (INT)0x00FF & (p.ring[i] >> (j * 8));
-        cout << v;
-    }
-    cout << endl;
-}
-cout << "---" << endl;
-*/
+        int part_size = N / K;
+        part_size = (N % K) ?  part_size + 1 : part_size;
+        int part_size_num = 0;
+        int part_size_small = 0;
+        int tmp_n = N;
+        int tmp_k = K;
+        while (tmp_k) {
+            int s = tmp_n % tmp_k ? 
+                    tmp_n / tmp_k + 1:
+                    tmp_n / tmp_k;
+            tmp_k -= 1;
+            tmp_n -= s;
+            part_size_num += (s == part_size);
+            part_size_small += (s != part_size);
+        }
 
 
         vector<int> indices;
-        indices.reserve(p.N);
-        for (int i = 0; i < p.N; ++i) {
+        indices.reserve(N);
+        for (int i = 0; i < N; ++i)
             indices.push_back(i);
-        }
-        sort(indices.begin(), indices.end(), idx_less(p));
-
-/*
-for (int z = 0; z < p.N; ++z) {
-    cout << z << "\t[" << indices[z] << "]\t";
-    int b = indices[z];
-    int e = (b + p.part_size) % p.N;
-    print(cout, p, b, e);
-}
-cout << endl;
-*/
+        sort(indices.begin(), indices.end(), idx_less(ring, N, part_size));
 
 
-        int min_beg = 0, 
-            min_end = 0;
+        int min_beg = 0, min_end = 0;
 
-        for (int i = 0; i < p.N; ++i) {
+        for (int i = 0; i < N; ++i) {
             int b = indices[i];
-            int e = (b + p.part_size) % p.N;
+//                b = i;
+            int  e = (b + part_size) % N; // increment(b, N, part_size);
 
-            int full_parts = p.part_size_num - 1;
-            int full_parts_small = p.part_size_small_num;
+//timer tm1("main loop");
+
+            if (min_beg != min_end) {
+//                if (ring[min_beg] < ring[b])
+//                    continue;
+//                else {
+                    int res = cmp(ring, N, min_beg, min_end, b, e/*, part_size*/);
+                    if (0 >= res)
+                        continue;
+//                }
+            }
+
+
+            //begins[0] = b;
+            //begins[1] = e;
+
+
+            int full_parts = part_size_num - 1;
+            int full_parts_small = part_size_small;
 
             int tmp_b = e;
-            int tmp_e = (tmp_b + p.part_size) % p.N;
+            int tmp_e = (tmp_b + part_size) % N;
+            int space = part_size;
 
 
             while (full_parts) {
+//timer tm2("nested loop");
 
-                int res = cmp(p, b, tmp_b);
+                int res = cmp(ring, N, b, e, tmp_b, tmp_e);
 
                 if (0 <= res) {
                     full_parts -= 1;
+                    space += part_size;
                     tmp_b = tmp_e;
                 }
                 else {
                     if (0 == full_parts_small)
                         break;
+                    space += part_size - 1;
                     full_parts_small -= 1;
-                    tmp_b = (tmp_b + (p.part_size - 1)) % p.N;
+                    tmp_b = (tmp_b + (part_size - 1)) % N;
                 }
-                tmp_e = (tmp_b + p.part_size) % p.N;
+                tmp_e = (tmp_b + part_size) % N;
 
             }   // while true
                 
             // last one partition
             if (0 == full_parts) {
+cout << "found: ";
+for (int z = b; z != e; z = (z + 1) % N)
+    cout << ring[z];
+cout << endl;
                 min_beg = b;
                 min_end = e; 
-                break;
-
-//                cout << "FOUND " << i << " [" << indices[i] << "] ";
-//                print(cout, p, b, e);
             }
-//            else {
-//                cout << "not found " << i << " [" << indices[i] << "] ";
-//                print(cout, p, b, e);
-//            }
+               
         }
 
 
         stringstream ss;
-        print(ss, p, min_beg, min_end);
-        cout << ss.str();
-    }
-
-}
-
-
-
-void prepare_params(params& p) {
-    p.part_size = p.N / p.K;
-    p.part_size = (p.N % p.K) ?  p.part_size + 1 : p.part_size;
-    p.part_size_num = 0;
-    p.part_size_small_num = 0;
-    int tmp_n = p.N;
-    int tmp_k = p.K;
-    while (tmp_k) {
-        int s = tmp_n % tmp_k ?
-                tmp_n / tmp_k + 1:
-                tmp_n / tmp_k;
-        tmp_k -= 1;
-        tmp_n -= s;
-        p.part_size_num += (s == p.part_size);
-        p.part_size_small_num += (s != p.part_size);
-    }
-
-    p.num_per_INT = p.N > sizeof(INT) ? sizeof(INT) : p.N;
-    p.num_per_INT = p.part_size > p.num_per_INT ? p.num_per_INT : p.part_size;
-//cout << "Num per INT: " << p.num_per_INT << endl;
-
-    int shift = sizeof(INT) - p.part_size;
-    INT tmp_mask = p.part_size <= sizeof(INT) ? ~((~INT(0) >> shift) << shift) : ~INT(0);
-    shift = (sizeof(INT) - p.part_size % sizeof(INT)) * 8;
-//cout << "shift " << shift << " so(" << sizeof(INT) << ")" << endl;
-   
-    p.last_mask = (tmp_mask >> shift) << shift;
-//cout << "Last mask: ";
-//for (int i = sizeof(INT)-1; i >= 0; --i) {
-//    INT v = (INT)0xFF & (p.last_mask >> (i * 8));
-//    cout << v;
-//}
-//cout << endl;
-
-    shift = p.part_size <= sizeof(INT) ? (p.part_size - 1) * 8 : (sizeof(INT) - 1) * 8;
-    p.first_mask = INT(0xFF) << shift; 
-//cout << "First mask: ";
-//for (int i = sizeof(INT)-1; i >= 0; --i) {
-//    INT v = (INT)0xFF & (p.first_mask >> (i * 8));
-//    cout << v;
-//}
-//cout << endl;
-
-
-    p.ring = new INT[p.N];
-    for (int i = 0; i < p.N; ++i)
-        p.ring[i] = 0;
     
-}
-
-void push_val(params& p, INT val, int idx) {
-    idx -= p.num_per_INT - 1;
-    idx = idx + p.N * ((unsigned int)idx >> (8 * sizeof(int) - 1));
-
-    for (int i = 0; i < p.num_per_INT; ++i) {
-        p.ring[idx] |= INT(val) << (i * 8);
-
-        idx = (idx + 1) % p.N;
+        for (;min_beg != min_end; min_beg=(min_beg + 1) % N)
+            ss << (int)ring[min_beg];
+        cout << ss.str() << endl;
     }
+
+//timer::print(cout);
 }
+
+
 
 
 int main(int argc, const char* argv[]) {
@@ -264,38 +262,20 @@ int main(int argc, const char* argv[]) {
     int N, K;
     cin >> N >> K;
 
-    params p;
-    p.N = N;
-    p.K = K;
-    prepare_params(p);
-
-
+    storage ring(N+16, 0);
+    size_t addr = 0;
+    while((size_t)&ring[addr] % 16)
+        addr += 1;
+    
     string line;
     cin >> line;
 
-    for (int i = 0; i < N; ++i) {
-        push_val(p, INT(line[i] - '0'), i);
+    for (int i = addr; i < (N+addr); ++i) {
+        ring[i] = int(line[i-addr] - '0');
     }
 
-    solve(p);
-/*
-    for (int i = 0; i < p.N; ++i) {
-        cout << "[" << i << "] ";
-        for (int j = sizeof(INT) - 1; j >= 0; --j) {
-            INT v = (INT)0x00FF & (p.ring[i] >> (j * 8));
-            cout << v;
-        }
-        cout << endl;
-    }
-    cout << "Last mask: ";
-    for (int j = sizeof(INT) - 1; j >= 0; --j) {
-        INT v = (INT)0x00FF & (p.last_mask >> (j * 8));
-        cout << v;
-    }
-    cout << endl;
-*/
+    solve<cmp_impl>(&ring[addr], N, K);
 
 
     return 0;
 }
-
